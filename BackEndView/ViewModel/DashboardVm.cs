fdfb73d366433924356ba0332d2d1c1de996d69d
stefa <steafanmensik1994@gmail.com>
2018-06-16 17:54:25 +0200

    some dashboard features

--- BackEndView/ViewModel/DashboardVm.cs
+++ BackEndView/ViewModel/DashboardVm.cs
@@ -1,6 +1,10 @@
-﻿using GalaSoft.MvvmLight;
+﻿using DataRepository;
+using GalaSoft.MvvmLight;
+using OxyPlot;
+using SharedClasses;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,77 @@ namespace BackEndView.ViewModel
 {
     public class DashboardVm : ViewModelBase
     {
+        private ObservableCollection<DataPoint> sales;
+
+        public ObservableCollection<DataPoint> Sales
+        {
+            get { return sales; }
+            set { sales = value; }
+        }
+
+        private int offeneBestellungen;
+
+        public int OffeneBestellungen
+        {
+            get { return offeneBestellungen; }
+            set { offeneBestellungen = value; }
+        }
+
+        private int verkaufteProdukte;
+
+        public int VerkaufteProdukte
+        {
+            get { return verkaufteProdukte; }
+            set { verkaufteProdukte = value; }
+        }
+
+
+        private ObservableCollection<SharedArticle> topProdukte;
+
+        public ObservableCollection<SharedArticle> TopProdukte
+        {
+            get { return topProdukte; }
+            set { topProdukte = value; }
+        }
+
+        private DataHandler dh;
+
+        public DashboardVm()
+        {
+
+            dh = new DataHandler();
+            InitialzeDashboard();
+            GenerateDemoData();
+
+        }
+
+        private void InitialzeDashboard()
+        {
+            OffeneBestellungen = dh.GetOrdersByStatus("offen").Count();
+        }
+
+        private void GenerateDemoData()
+        {
+            Sales = new ObservableCollection<DataPoint>
+            {
+                new DataPoint(0,0),
+                new DataPoint(1,12000),
+                new DataPoint(2,15000),
+                new DataPoint(3,16000),
+                new DataPoint(4, 22000),
+                new DataPoint(5,27000),
+                new DataPoint(6,20000),
+                new DataPoint(7,19000),
+                new DataPoint(8,26000),
+                new DataPoint(9,29000),
+                new DataPoint(10,32000),
+                new DataPoint(11,45000),
+                new DataPoint(12,57000)
+
+
+            };
+
+
+        }
     }
 }
---
+++ DataAggregator/DataAggregator.cs
@@ -0,0 +1,47 @@
+﻿using DataRepository;
+using SharedClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAggregator
+{
+    public static class DataAggregator
+    {
+        private static DataHandler dh =  new DataHandler();
+
+        public static void GetSalesPerMonth()
+        {
+
+            List <SharedBestellung> orders = dh.GetOrdersByStatus("abgeschlossen");
+            Dictionary<int, double> sales = new Dictionary<int, double>();
+            int month = 0;
+            double orderVal = 0;
+
+            foreach (var order in orders)
+            {
+                month = order.LieferDatum.Value.Month;
+                orderVal = order.GesamtSumme.Value;
+
+                if (!sales.ContainsKey(month))
+                {
+                    sales.Add(month, orderVal);
+                }
+                else
+                {
+                    sales[month] = sales[month] + orderVal;
+
+                }
+                
+            }
+
+
+
+
+        }
+
+
+    }
+}
