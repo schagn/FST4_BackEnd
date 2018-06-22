1482b9527fd3b437646253f8d73e616c26964f6b
stefa <steafanmensik1994@gmail.com>
2018-06-22 08:36:42 +0200

    testcode

--- TEST/Program.cs
+++ TEST/Program.cs
@@ -1,9 +1,12 @@
 ﻿using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+using OfficeOpenXml.Style;
 using OfficeOpenXml.Table;
 using SharedClasses;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,15 +20,27 @@ namespace TEST
         {
 
 
-            DataTable table = DataAggregator.DataAggregator.getRawMaterialConsumptionOfMonth(2);
+            DataTable table = DataAggregator.DataAggregator.getRawMaterialConsumptionOfMonth(4);
+
 
             
 
             using (ExcelPackage pck = new ExcelPackage())
             {
                 ExcelWorksheet ws = pck.Workbook.Worksheets.Add("Accounts");
-                ws.Cells["A1"].LoadFromDataTable(table, true, TableStyles.Light17);
+                ws.Cells["A7"].LoadFromDataTable(table, true, TableStyles.Light17);
+                Image img = Image.FromFile(@"C:\Users\stefa\Desktop\GetYourCake_logo.PNG");
+                int RowIndex = 0;
+                int ColIndex = 0;
+                ExcelPicture pic = ws.Drawings.AddPicture("Logo", img);
+                pic.SetPosition(RowIndex, 0, ColIndex, 0);
                 ws.Cells.AutoFitColumns();
+                ws.Column(1).Width = 18.93;
+                ws.Column(2).Width = 22.27;
+                ws.Column(3).Width = 18.47;
+                ws.Cells["A7:C7"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+
                 pck.Save();
 
                 byte[] data = pck.GetAsByteArray();
